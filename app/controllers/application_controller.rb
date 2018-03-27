4ed04b6db75ba07bc5e66ac1e3a0b14a31ce43dd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-27 17:41:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,12 +30,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id/edit' do
-    @post = Post.find_by_id(params[:id])
+    @post = Post.find(params[:id])
     erb :edit
   end
 
   patch '/posts/:id' do
-    @post = Post.find_by_id(params[:id])
+    @post = Post.find(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
