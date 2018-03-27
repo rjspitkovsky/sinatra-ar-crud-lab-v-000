cb802b97268698c5c7fcdea13915a965b2d64695
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-27 17:42:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,4 +41,9 @@ class ApplicationController < Sinatra::Base
     @post.save
     erb :show
   end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+  end 
 end
